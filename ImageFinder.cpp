e9fb4788186d21bb054b7aca9d33c814ac7e667f
Ruslan Kabatsayev <b7.10110111@gmail.com>
2025-07-28 10:32:59 +0800

    Use exiv2 to find image date-time

--- ImageFinder.cpp
+++ ImageFinder.cpp
@@ -8,6 +8,88 @@
 #ifdef Q_OS_ANDROID
 #include <QtCore/private/qandroidextras_p.h>
 #endif
+#include <exiv2/exiv2.hpp>
+
+namespace
+{
+QDateTime getFallbackDateTime(const QString& path)
+{
+    qWarning() << "Resorting to file date-time for" << path;
+    const QFileInfo info(path);
+    auto dateTime = info.birthTime();
+    if(!dateTime.isValid())
+        dateTime = info.lastModified();
+    return dateTime;
+}
+
+QDateTime getDateTime(const QString& path)
+{
+    const auto image = Exiv2::ImageFactory::open(path.toStdString());
+    if(!image.get())
+    {
+        qWarning() << "exiv2 read failed for" << path;
+        return getFallbackDateTime(path);
+    }
+    image->readMetadata();
+    const auto& exif = image->exifData();
+
+    QString date;
+    for(const auto& key : {"Exif.Photo.DateTimeOriginal",
+                           "Exif.Image.DateTimeOriginal",
+                           "Exif.Image.DateTime"})
+    {
+        const auto it=exif.findKey(Exiv2::ExifKey(key));
+        if(it!=exif.end())
+        {
+            date = QString::fromStdString(it->toString());
+            break;
+        }
+    }
+    QString gpsTime;
+    for(const auto& key : {"Exif.GPSInfo.GPSTimeStamp"})
+    {
+        const auto it=exif.findKey(Exiv2::ExifKey(key));
+        if(it!=exif.end())
+        {
+            if(it->count() != 3)
+                continue;
+            const auto hour = it->toFloat(0);
+            const auto min = it->toFloat(1);
+            const auto sec = it->toFloat(2);
+            if(hour < 0 || hour > 59 || hour - std::floor(hour) != 0 ||
+               min  < 0 || min  > 59 || min  - std::floor(min ) != 0)
+                continue;
+            gpsTime = QString("%1:%2:%3").arg(int(hour), 2, 10, QChar('0'))
+                .arg(int(min), 2, 10, QChar('0'))
+                .arg(double(sec), 2, 'f', 0, QChar('0'));
+            break;
+        }
+    }
+
+    QString gpsDate;
+    for(const auto& key : {"Exif.GPSInfo.GPSDateStamp"})
+    {
+        const auto it=exif.findKey(Exiv2::ExifKey(key));
+        if(it!=exif.end())
+        {
+            gpsDate = QString::fromStdString(it->toString());
+            break;
+        }
+    }
+
+    // If GPS date and time are present, take them as more reliable.
+    // At the very least, time zone is present there unconditionally.
+    if(!gpsDate.isEmpty() && !gpsTime.isEmpty())
+    {
+        date = gpsDate + " " + gpsTime;
+    }
+
+    const auto dateTime = QDateTime::fromString(date, "yyyy:MM:dd HH:mm:ss");
+    if(dateTime.isValid()) return dateTime;
+
+    return getFallbackDateTime(path);
+}
+}
 
 ImageFinder::ImageFinder(const int thumbnailWidth, QObject* parent)
     : QThread(parent)
@@ -53,12 +135,7 @@ void ImageFinder::findAllImages()
             if(size.width() != size.height() * 2)
                 continue;
 
-            // FIXME: should get it from EXIF, otherwise this may be wrong
-            // (e.g. when an image was downloaded from Telegram into Gallery).
-            const QFileInfo info(path);
-            auto dateTime = info.birthTime();
-            if(!dateTime.isValid())
-                dateTime = info.lastModified();
+            const auto dateTime = getDateTime(path);
 
             imageInfos_.push_back({path, dateTime});
             emit imageFound(imageInfos_.back());
